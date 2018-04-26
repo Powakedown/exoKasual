e3b755de2024bafad9a9ff190771374d0998acf0
Maxime Boue <abriko@yahoo.fr>
2018-04-26 16:12:05 +0200

    Add Ajax to update details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
   def home; end
 
-  def bars
-    query = params[:query][:business]
-    search(query)
+  def bars_map
+    @query = params[:query] ? params[:query][:business] : session[:query]
+    session[:query] = @query
+    @bars = search(@query)
+    @markersLatLng = @bars["businesses"].map { |bar| [bar["coordinates"]["latitude"],bar["coordinates"]["longitude"]]}
+  end
+
+  def bars_list
+    @query = session[:query]
+    @bars = search(@query)["businesses"]
   end
 
   def search(term)
@@ -17,9 +24,10 @@ class PagesController < ApplicationController
     }
 
     response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get(url, params: params)
-    barsArray = response.parse
+    response.parse
+  end
 
-    @barsGeoloc = barsArray["businesses"].map { |bar| [bar["coordinates"]["latitude"],bar["coordinates"]["longitude"]]}
-
+  def update_details
+    @bar = params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  get "/bars", to: "pages#bars"
+  get "/bars-map", to: "pages#bars_map"
+  get "/bars-list", to: "pages#bars_list"
+  get "/bar-details", to: "pages#update_details"
 end
